21dcbe0cda6b3ac11761319fc721b6d7c26e67fd
zhuzhiyuan <ly_zzy@126.com>
2016-07-20 22:45:15 +0800

    增加了超链接功能

---
+++ Caijiqi/Business/Common.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Net;
+
+namespace Caijiqi.Business
+{
+    public class Common
+    {
+        public static string Get(string url,string data=default(string))
+        {
+            string result = string.Empty;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
+                request.Accept = "text/plain";
+                request.ContentType = "application/json;charset=UTF-8";
+                request.Method = "GET";
+                request.Headers.Add("Accept-Encoding", "gzip,deflate");
+                request.Referer = "http://pub.alimama.com/";
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                if (response.ContentEncoding == "gzip")
+                {
+                    MemoryStream ms = new MemoryStream();
+                    GZipStream zip = new GZipStream(response.GetResponseStream(), CompressionMode.Decompress);
+                    byte[] buffer = new byte[1024];
+                    int l = zip.Read(buffer, 0, buffer.Length);
+                    while (l > 0)
+                    {
+                        ms.Write(buffer, 0, l);
+                        l = zip.Read(buffer, 0, buffer.Length);
+                    }
+                    ms.Dispose();
+                    zip.Dispose();
+                    result = Encoding.UTF8.GetString(ms.ToArray());
+                    return result;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return result;
+        }
+
+        public static string Post(string url, string data)
+        {
+            string result = string.Empty;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Accept = "text/plain";
+                request.ContentType = "application/json;charset=UTF-8";
+                request.Method = "GET";
+                request.Headers.Add("Accept-Encoding", "gzip,deflate");
+                request.Referer = "http://pub.alimama.com/";
+                request.ContentLength = data.Length;
+                Stream postStream = request.GetRequestStream();
+                byte[] postData = Encoding.UTF8.GetBytes(data);
+                postStream.Write(postData, 0, postData.Length);
+                postStream.Dispose();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return result;
+        }
+    }
+}
---
+++ Caijiqi/FrmWebSpy.cs
@@ -0,0 +1,160 @@
+/********************************************************************
+ * *
+ * * 使本项目源码或本项目生成的DLL前请仔细阅读以下协议内容，如果你同意以下协议才能使用本项目所有的功能，
+ * * 否则如果你违反了以下协议，有可能陷入法律纠纷和赔偿，作者保留追究法律责任的权利。
+ * *
+ * * 1、你可以在开发的软件产品中使用和修改本项目的源码和DLL，但是请保留所有相关的版权信息。
+ * * 2、不能将本项目源码与作者的其他项目整合作为一个单独的软件售卖给他人使用。
+ * * 3、不能传播本项目的源码和DLL，包括上传到网上、拷贝给他人等方式。
+ * * 4、以上协议暂时定制，由于还不完善，作者保留以后修改协议的权利。
+ * *
+ * * Copyright (C) 2013-? cskin Corporation All rights reserved.
+ * * 网站：CSkin界面库 http://www.cskin.net 论坛 http://bbs.cskin.net
+ * * 作者： 乔克斯 QQ：345015918 .Net项目技术组群：306485590
+ * * 请保留以上版权信息，否则作者将保留追究法律责任。
+ * *
+ * * 创建时间：2014-08-26
+ * * 说明：FrmWebSpy.cs
+ * *
+********************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Caijiqi
+{
+    public partial class FrmWebSpy : Form
+    {
+        public FrmWebSpy() {
+            InitializeComponent();
+            SetStyles();
+            InitComBoxData();
+        }
+        #region 减少闪烁
+        //减少闪烁
+        private void SetStyles() {
+            base.SetStyle(
+              ControlStyles.UserPaint |
+              ControlStyles.AllPaintingInWmPaint |
+              ControlStyles.OptimizedDoubleBuffer |
+              ControlStyles.ResizeRedraw |
+              ControlStyles.DoubleBuffer, true);
+            base.UpdateStyles();
+            
+            base.AutoScaleMode = AutoScaleMode.None;
+        }
+        #endregion
+
+        private void InitComBoxData()
+        {
+            skinComboBox1.Items.AddRange(new object[]
+            {
+                "默认排序", "收入比率", "人气", "价格从高到低", "价格从低到高", "月推广量", "", "月支出佣金", "", "销量"
+            });
+            skinComboBox1.SelectedIndex = 0;
+        }
+
+
+        private void btnCaiji_Click(object sender, EventArgs e)
+        {
+            string key = skinTextBox1.SkinTxt.Text;
+            string response = Business.Common.Get(
+                "http://pub.alimama.com/items/search.json?q="+System.Web.HttpUtility.UrlEncode(key)+
+                "&_t=1468998050262&toPage=1&queryType=0&sortType="+skinComboBox1.SelectedIndex+
+                "&auctionTag=&perPageSize=100&shopTag=&t=1468998639277&_tb_token_=test&pvid=10_203.156.203.9_2701_1468998639018");
+            List<string> param = new List<string>()
+            {
+                "q="+System.Web.HttpUtility.UrlEncode(key),
+                "_t="+DateTime.Now.Ticks,
+                "sortType="+skinComboBox1.SelectedIndex,
+                "perPageSize=40"
+            };
+            if (skinCheckBox1.Checked)
+            {
+                param.Add("hPayRate30=1");
+            }
+            if (skinCheckBox2.Checked)
+            {
+                param.Add("b2c=1");
+            }
+            if (!string.IsNullOrEmpty(startBiz30day.SkinTxt.Text))
+            {
+                param.Add("startBiz30day=" + startBiz30day.SkinTxt.Text);
+            }
+            if (!string.IsNullOrEmpty(startTkRate.SkinTxt.Text))
+            {
+                param.Add("startTkRate="+startTkRate.SkinTxt.Text);
+            }
+
+            if (!string.IsNullOrEmpty(startPrice.SkinTxt.Text))
+            {
+                param.Add("startPrice=" + startPrice.SkinTxt.Text);
+            }
+            if (!string.IsNullOrEmpty(endPrice.SkinTxt.Text))
+            {
+                param.Add("endPrice=" + endPrice.SkinTxt.Text);
+            }
+            int page = !string.IsNullOrEmpty(pageSize.SkinTxt.Text) ? int.Parse(pageSize.SkinTxt.Text) : 1;
+            string url = "http://pub.alimama.com/items/search.json?";
+            int m = !string.IsNullOrEmpty(month.SkinTxt.Text) ? int.Parse(month.SkinTxt.Text) : 0;
+            skinDataGridView4.Rows.Clear();
+            for (int i = 0; i < page; i++)
+            {
+                url += url = string.Join("&", param.ToArray()) + "&toPage=" + (i + 1);
+                JObject json = JsonConvert.DeserializeObject<JObject>(response);
+
+                JArray pageList = (json["data"] as JObject)["pageList"] as JArray;
+
+                foreach (JObject item in pageList)
+                {
+                    int totalNum = 0;
+                    int.TryParse(item["totalNum"].ToString(), out totalNum);
+                    if (totalNum > m)
+                    {
+                        string title = item["title"].ToString();
+                        string strText = System.Text.RegularExpressions.Regex.Replace(title, "<[^>]+>", "");
+                        strText = System.Text.RegularExpressions.Regex.Replace(strText, "&[^;]+;", "");
+                        skinDataGridView4.Rows.Add(new object[]
+                        {strText, item["auctionUrl"].ToString(), item["nick"].ToString()});
+                    }
+                }
+            }
+        }
+
+        private void Number_SkinTxt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != 8 && !Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void skinDataGridView4_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 1)
+            {
+                System.Diagnostics.Process.Start((sender as DataGridView).Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString());
+            }
+        }
+    }
+
+    enum Sort
+    {
+        def=0,
+        shourubilv=1,
+        renqi=2,
+        jiageDesc=3,
+        jiageAsc=4,
+        yuetuiguangliang=5,
+        yuezhichuyongjin=7,
+        xiaoliangDesc=9,
+    }
+}
